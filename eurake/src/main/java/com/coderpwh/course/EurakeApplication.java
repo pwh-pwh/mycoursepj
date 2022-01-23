44686a76bee2a9465dc5c146b54ff9bf39a12784
pwh-pwh <plmoknpwh@163.com>
2022-01-23 09:59:23 +0800

    初始化course项目

---
+++ eurake/src/main/java/com/coderpwh/course/EurakeApplication.java
@@ -0,0 +1,11 @@
+package com.coderpwh.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurakeApplication.class,args);
+    }
+}
