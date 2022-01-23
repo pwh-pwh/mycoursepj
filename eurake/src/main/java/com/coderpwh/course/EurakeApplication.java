2b34df4de18a1bbd7fa5eef49f913ff1fcbb5a27
pwh-pwh <plmoknpwh@163.com>
2022-01-23 10:21:41 +0800

    1.优化日志 2.配置eurake

--- eurake/src/main/java/com/coderpwh/course/EurakeApplication.java
+++ eurake/src/main/java/com/coderpwh/course/EurakeApplication.java
@@ -2,8 +2,10 @@ package com.coderpwh.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurakeApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurakeApplication.class,args);
