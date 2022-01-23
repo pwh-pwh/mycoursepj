4f612cedd5eb8f8c53e617994d122dac04a67738
pwh-pwh <plmoknpwh@163.com>
2022-01-23 16:48:37 +0800

    1.将持久化框架改为mybatis-plus

--- server/src/main/java/com/coderpwh/server/mapper/TestMapper.java
+++ server/src/main/java/com/coderpwh/server/mapper/TestMapper.java
@@ -2,10 +2,11 @@ package com.coderpwh.server.mapper;
 
 
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.coderpwh.server.domain.Test;
 
 import java.util.List;
 
-public interface TestMapper {
+public interface TestMapper extends BaseMapper<Test> {
     List<Test> testList();
 }
