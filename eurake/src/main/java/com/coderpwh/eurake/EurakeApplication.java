1b286126bd0fd406613095d546e27ac30bd111cb
pwh-pwh <plmoknpwh@163.com>
2022-01-23 10:57:40 +0800

    1.增加gateway模块

--- eurake/src/main/java/com/coderpwh/eurake/EurakeApplication.java
+++ eurake/src/main/java/com/coderpwh/eurake/EurakeApplication.java
@@ -1,4 +1,4 @@
-package com.coderpwh.course;
+package com.coderpwh.eurake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
