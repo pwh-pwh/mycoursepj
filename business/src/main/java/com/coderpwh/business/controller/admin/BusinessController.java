d715760aac7e297f3e597ad9445cb190d713eb60
pwh-pwh <plmoknpwh@163.com>
2022-01-25 17:40:17 +0800

    1.新增增加chapter接口

--- business/src/main/java/com/coderpwh/business/controller/admin/BusinessController.java
+++ business/src/main/java/com/coderpwh/business/controller/admin/BusinessController.java
@@ -1,22 +1,34 @@
 package com.coderpwh.business.controller.admin;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.coderpwh.server.dto.ChapterDto;
 import com.coderpwh.server.service.ChapterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/chapter")
 public class BusinessController {
 
+    private static final Logger log = LoggerFactory.getLogger(BusinessController.class);
     @Resource
     private ChapterService chapterService;
 
     @RequestMapping("/list")
-    public List<ChapterDto> chapterList() {
-        return chapterService.findAllChapter();
+    public Page<ChapterDto> chapterList(@RequestBody Page<ChapterDto> page) {
+
+        return chapterService.findAllChapter(page);
+    }
+
+    @PostMapping("/save")
+    public ChapterDto saveChapter(@RequestBody ChapterDto chapterDto) {
+        log.debug("{}",chapterDto);
+        chapterService.save(chapterDto);
+        return chapterDto;
     }
 }
